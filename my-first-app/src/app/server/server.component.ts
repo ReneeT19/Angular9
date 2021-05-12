89ae4205c1dba8426914fffd5cd1556c59b52713
Renee Thomsen <rcthomsen19@gmail.com>
2021-05-12 18:25:38 -0400

    added event binding

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -41,4 +41,16 @@ export class ServerComponent  {
     }, 2000);
   }
 
+  //event binding
+  serverCreationStatus = 'No server was created!';
+  serverName = "";
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server was created!";
+  }
+
+  onUpdateServerName(event: Event) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
