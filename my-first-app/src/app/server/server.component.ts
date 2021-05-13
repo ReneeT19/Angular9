a7f089bfd815657d421bb22fbfeb2d902154b8e7
Renee Thomsen <rcthomsen19@gmail.com>
2021-05-13 12:17:51 -0400

    code refactored

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -11,63 +11,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent  {
 
-  name = "";
-  serverId: number;
-  isOnline: boolean;
-  isSubmitted = false;
-  userName = "";
-  serverCreated = false;
-  //to use ngFor instead of calling <app-server> twice in servers.component.html
-  servers = ['Testserver', 'Testserver 2'];
+  // name = "";
+  // serverId: number;
+  // isOnline: boolean;
+  // isSubmitted = false;
+  // userName = "";
+  // serverCreated = false;
+  // //to use ngFor instead of calling <app-server> twice in servers.component.html
+  // servers = ['Testserver', 'Testserver 2'];
 
-  getServerStatus() {
-    this.isSubmitted = true;
-    //only if you want to generate random numbers use the code below
-    // this.serverId = Math.floor(Math.random() *100)
-    if(this.serverId > 10) {
-      this.isOnline = true;
-    } else {
-      this.isOnline = false;
-    }
-  }
+  // getServerStatus() {
+  //   this.isSubmitted = true;
+  //   //only if you want to generate random numbers use the code below
+  //   // this.serverId = Math.floor(Math.random() *100)
+  //   if(this.serverId > 10) {
+  //     this.isOnline = true;
+  //   } else {
+  //     this.isOnline = false;
+  //   }
+  // }
 
   //below is from the tutorial for string interpolation
   serverId2: number = 10;
   serverStatus: string = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online': 'offline';
+  }
   //you can call a method as well but you need to make sure you return a string or something can be converted to a string
-  getServerStatus2() {
+  getServerStatus() {
     return this.serverStatus;
   }
   
   getColor() {
     return this.serverStatus === 'online' ? 'green' : 'red';
   }
+
+
   //property binding
-  allowNewServer = false;
+  // allowNewServer = false;
 
-  constructor() {
-    this.serverStatus = Math.random() > 0.5 ? 'online': 'offline';
+    // constructor(){
+    //   setTimeout(() => {
+    //     this.allowNewServer = true;
+    //   }, 2000);
+    // }
 
-    setTimeout(() => {
-      this.allowNewServer = true;
-    }, 2000);
-  }
 
   //event binding
-  serverCreationStatus = 'No server was created!';
-  // serverName = "";
-  serverName = "two-way binding"; //  two-way binding
+  // serverCreationStatus = 'No server was created!';
+  // // serverName = "";
+  // serverName = "two-way binding"; //  two-way binding
 
-  onCreateServer() {
-    this.serverCreated = true;
-    //to use ngFor
-    this.servers.push(this.serverName);
-    this.serverCreationStatus = "Server was created and the name is " + this.serverName;
-  }
+  // onCreateServer() {
+  //   this.serverCreated = true;
+  //   //to use ngFor
+  //   this.servers.push(this.serverName);
+  //   this.serverCreationStatus = "Server was created and the name is " + this.serverName;
+  // }
 
-  onUpdateServerName(event: Event) {
-    console.log(event);
-    this.serverName = (<HTMLInputElement>event.target).value;
-  }
+  // onUpdateServerName(event: Event) {
+  //   console.log(event);
+  //   this.serverName = (<HTMLInputElement>event.target).value;
+  // }
 }
--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -25,29 +25,32 @@ export class ServersComponent {
   isSubmitted = false;
   allowNewServer = false;
   serverStatus: string = 'offline';
-  
-  constructor() {
-    this.serverStatus = Math.random() > 0.5 ? 'online': 'offline';
 
+  constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
   }
-  getServerStatus() {
-    this.isSubmitted = true;
-    //only if you want to generate random numbers use the code below
-    // this.serverId = Math.floor(Math.random() *100)
-    if(this.serverId > 10) {
-      this.isOnline = true;
-    } else {
-      this.isOnline = false;
-    }
-  }
+
+  // getServerStatus() {
+  //   this.isSubmitted = true;
+  //   //only if you want to generate random numbers use the code below
+  //   // this.serverId = Math.floor(Math.random() *100)
+  //   if(this.serverId > 10) {
+  //     this.isOnline = true;
+  //   } else {
+  //     this.isOnline = false;
+  //   }
+  // }
 
   onCreateServer() {
     this.serverCreated = true;
     //to use ngFor
     this.servers.push(this.serverName);
     this.serverCreationStatus = "Server was created and the name is " + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
