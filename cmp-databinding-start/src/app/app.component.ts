730bd20b25bb6abb4c78122b83ce5db72847a7a5
Renee Thomsen <rcthomsen19@gmail.com>
2021-05-14 15:58:14 -0400

    child talks to parent

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -17,5 +17,21 @@ export class AppComponent {
     content: 'just a test'
   }
 ];
+
+onServerAdded(serverData: {serverName: string, serverContent: string}) {
+  this.serverElements.push({
+    type: 'server',
+    name: serverData.serverName,
+    content: serverData.serverContent
+  });
+}
+
+onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+  this.serverElements.push({
+    type: 'blueprint',
+    name: blueprintData.serverName,
+    content: blueprintData.serverContent
+  });
+}
   
 }
