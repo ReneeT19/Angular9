4e8942b1720357bb7916fbb727b80b3259eaed65
Renee Thomsen <rcthomsen19@gmail.com>
2021-05-14 13:38:30 -0400

    set up parent-child communication

---
+++ cmp-databinding-start/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverElements = [{
+    type: 'server',
+    name: 'testserver',
+    content: 'just a test'
+  },
+  {
+    type: 'blueprint',
+    name: 'blueprintserver',
+    content: 'just a test'
+  }
+];
+  
+}
