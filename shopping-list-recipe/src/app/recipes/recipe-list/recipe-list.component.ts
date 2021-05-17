74be3dfe1444e9488103d3070f31fe5b7df05775
Renee Thomsen <rcthomsen19@gmail.com>
2021-05-16 22:09:42 -0400

    added event and property binding

--- shopping-list-recipe/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-list-recipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,10 +7,11 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('Hop Pot', 'This is a traditional Chinese food', 'https://news.cgtn.com/news/3d3d514d3551544d31457a6333566d54/img/d28dd1786f3e4bb598da6e577b369898/d28dd1786f3e4bb598da6e577b369898.jpg'),
-    new Recipe('Hop Pot', 'This is a traditional Chinese food', 'https://news.cgtn.com/news/3d3d514d3551544d31457a6333566d54/img/d28dd1786f3e4bb598da6e577b369898/d28dd1786f3e4bb598da6e577b369898.jpg'),
-    new Recipe('Hop Pot', 'This is a traditional Chinese food', 'https://news.cgtn.com/news/3d3d514d3551544d31457a6333566d54/img/d28dd1786f3e4bb598da6e577b369898/d28dd1786f3e4bb598da6e577b369898.jpg')
+    new Recipe('Hop Pot', 'This is traditional Chinese food', 'https://news.cgtn.com/news/3d3d514d3551544d31457a6333566d54/img/d28dd1786f3e4bb598da6e577b369898/d28dd1786f3e4bb598da6e577b369898.jpg'),
+    new Recipe('Dandan Noodle', 'This is spicy Sichuan food', 'https://i1.wp.com/eatup.kitchen/wp-content/uploads/2017/06/DSCF2620.jpg'),
+    new Recipe('Fish blended with bok choy', 'This is what made Soren vomit', 'https://www.deegourmetgoddess.com/wp/wp-content/uploads/2017/10/1-steamed-ginger-shallot-fish-67005-1-640x400.jpg')
 
   ];
 
@@ -19,4 +20,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
