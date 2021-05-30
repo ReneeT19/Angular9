c15e2f68bbdcdad01e5b3b54f112db3620878e0c
Renee Thomsen <rcthomsen19@gmail.com>
2021-05-29 20:22:34 -0400

    added shoppinglist service

--- shopping-list-recipe/src/app/recipes/recipe.model.ts
+++ shopping-list-recipe/src/app/recipes/recipe.model.ts
@@ -1,14 +1,19 @@
 //blueprint for the recipe object we create using vanila TS
+
+import { Ingredient } from "../shared/ingredient.model";
+
 //define how a recipe should look like
 export class Recipe {
     public name: string;
     public description: string;
     public imagePath: string;
+    public ingredients: Ingredient[];
 //instantiate with new keyword and pass the args to the constructor
 //executed once creating a new instance of the class
-    constructor(name:string, desc: string, imagePath: string){
+    constructor(name:string, desc: string, imagePath: string, ingredients: Ingredient[]){
         this.name = name;
         this.description = desc;
         this.imagePath = imagePath;
+        this.ingredients = ingredients;
     }
 }
--- shopping-list-recipe/src/app/shopping-list/shopping-list.service.ts
+++ shopping-list-recipe/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,28 @@
+import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+
 export class ShoppingListService {
-    
+ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+ private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+    ];
+
+    getIngredients() {
+        return this.ingredients.slice();
+    }
+    addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
+
+    addIngredients(ingredients: Ingredient[]) {
+        // can use the push instead of the for loop
+        // for(let ingredient of ingredients) {
+        //     this.addIngredient(ingredient);
+        // }
+        this.ingredients.push(...ingredients); //push will push array as a single object but use ... spreads each ingredient to a list
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
 }
