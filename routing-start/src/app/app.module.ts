a4bbcadaa0618b2dd773597afcde7944fd0f81e5
Renee Thomsen <rcthomsen19@gmail.com>
2021-06-06 22:07:42 -0400

    added deactivate service

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
 import { AuthGuardService } from './auth-guard.service';
 import { AuthService } from './auth.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 
 // const appRoutes: Routes = [
 //   { path: '', component: HomeComponent},
@@ -47,7 +48,7 @@ import { AuthService } from './auth.service';
     AppRoutingModule
     // RouterModule.forRoot(appRoutes),
   ],
-  providers: [ServersService, AuthGuardService, AuthService],
+  providers: [ServersService, AuthGuardService, AuthService, CanDeactivateGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
