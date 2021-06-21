4ebd3fb59b1e07afd1245c2e2dfb10d1c6fba7bf
Renee Thomsen <rcthomsen19@gmail.com>
2021-06-21 12:25:57 -0400

    added subject

---
+++ obs-01-start/src/app/user/user.service.ts
@@ -0,0 +1,8 @@
+import { Injectable, EventEmitter } from "@angular/core";
+import { Subject } from "rxjs";
+
+
+@Injectable()
+export class UserService {
+    activatedEmitter = new Subject<boolean>();
+}
