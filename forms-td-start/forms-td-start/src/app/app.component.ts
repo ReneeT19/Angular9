bb97b3b6a84ccda752fbf686b1bd7151b5a6beb8
Renee Thomsen <rcthomsen19@gmail.com>
2021-06-22 16:38:23 -0400

    added data binding

--- forms-td-start/forms-td-start/src/app/app.component.ts
+++ forms-td-start/forms-td-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,12 +7,21 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'teacher';
+  answer = '';
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
-  onSubmit(form: NgForm) {
-    // console.log('submitted');
-    console.log(form);
+  // onSubmit(form: NgForm) {
+  //   // console.log('submitted');
+  //   console.log(form);
+  // }
+
+  // an alternative is to use viewchild to access local template variable without passing f in the html
+  onSubmit() {
+    console.log(this.signupForm);
   }
 }
