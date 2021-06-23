fb8a69827f127d0d1c67ff69d5c9875b6249f90f
Renee Thomsen <rcthomsen19@gmail.com>
2021-06-23 17:16:17 -0400

    finished td form

--- forms-td-start/forms-td-start/src/app/app.component.ts
+++ forms-td-start/forms-td-start/src/app/app.component.ts
@@ -10,9 +10,33 @@ export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultQuestion = 'teacher';
   answer = '';
+  genders = ['male', 'female'];
+  // create a user property that's a new property, not necessarily matching the js object
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm) {
@@ -23,5 +47,15 @@ export class AppComponent {
   // an alternative is to use viewchild to access local template variable without passing f in the html
   onSubmit() {
     console.log(this.signupForm);
+    this.submitted = true;
+    // the value on the right side needs to match whatever we gave in the html value name
+    //update user information after you submit the form
+    this.user.username = this.signupForm.value.userData.username; //valid value on userData grouping
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    // 
+    this.signupForm.reset();
   }
 }
