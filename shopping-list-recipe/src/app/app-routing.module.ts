b10b1e96652c2373ebb2f5a77dae1e297233baa9
Renee Thomsen <rcthomsen19@gmail.com>
2021-09-20 21:16:03 -0400

    splitted modules

--- shopping-list-recipe/src/app/app-routing.module.ts
+++ shopping-list-recipe/src/app/app-routing.module.ts
@@ -11,12 +11,12 @@ import { AuthGuard } from "./auth/auth/auth.guard";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' }, //only redirect when the full path is empty
-    { path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard], children: [
-        { path: '', component: RecipeStartComponent },
-        { path: 'new', component: RecipeEditComponent},
-        { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService] },
-        { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService]}
-    ]},
+    // { path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard], children: [
+    //     { path: '', component: RecipeStartComponent },
+    //     { path: 'new', component: RecipeEditComponent},
+    //     { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService] },
+    //     { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService]}
+    // ]},
     { path: 'shopping-list', component: ShoppingListComponent},
     { path: 'auth', component: AuthComponent }
 ];
--- shopping-list-recipe/src/app/app.module.ts
+++ shopping-list-recipe/src/app/app.module.ts
@@ -4,37 +4,38 @@ import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+// import { RecipesComponent } from './recipes/recipes.component';
+// import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+// import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+// import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+// import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+// import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AlertComponent } from './alert/alert.component';
+import { RecipesModule } from './recipes/recipes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
+    // RecipesComponent,
+    // RecipeListComponent,
+    // RecipeDetailComponent,
+    // RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
+    // RecipeStartComponent,
+    // RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
     AlertComponent
@@ -44,7 +45,8 @@ import { AlertComponent } from './alert/alert.component';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RecipesModule
   ],
   providers: [ShoppingListService, RecipeService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
   bootstrap: [AppComponent]
