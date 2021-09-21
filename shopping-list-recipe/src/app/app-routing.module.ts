f369bcdb0e08ba6ce6a7d1668011d0d7ff4ee5b0
Renee Thomsen <rcthomsen19@gmail.com>
2021-09-21 12:17:18 -0400

    created shoppinglist module

--- shopping-list-recipe/src/app/app-routing.module.ts
+++ shopping-list-recipe/src/app/app-routing.module.ts
@@ -1,11 +1,5 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
-import { RecipesComponent } from "./recipes/recipes.component";
-import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-import {RecipesResolverService} from './recipes/recipes-resolver.service';
 import { AuthComponent } from "./auth/auth/auth.component";
 import { AuthGuard } from "./auth/auth/auth.guard";
 
@@ -17,7 +11,7 @@ const appRoutes: Routes = [
     //     { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService] },
     //     { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService]}
     // ]},
-    { path: 'shopping-list', component: ShoppingListComponent},
+    // { path: 'shopping-list', component: ShoppingListComponent},
     { path: 'auth', component: AuthComponent }
 ];
 
--- shopping-list-recipe/src/app/recipes/recipes.module.ts
+++ shopping-list-recipe/src/app/recipes/recipes.module.ts
@@ -9,6 +9,7 @@ import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
 import { RecipesRoutingModule } from "./recipes-routing.module";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
     declarations: [
@@ -20,7 +21,7 @@ import { RecipesRoutingModule } from "./recipes-routing.module";
     RecipeEditComponent,
     ],
     imports: [
-        RouterModule,CommonModule, ReactiveFormsModule, RecipesRoutingModule
+        RouterModule, ReactiveFormsModule, RecipesRoutingModule, SharedModule
     ]
     // exports: [
     // RecipesComponent,
