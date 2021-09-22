7792432e8a927379d1e7698af9fb0f3f6a67e4e9
Renee Thomsen <rcthomsen19@gmail.com>
2021-09-21 21:59:13 -0400

    fixed loadChildren code

--- shopping-list-recipe/src/app/app-routing.module.ts
+++ shopping-list-recipe/src/app/app-routing.module.ts
@@ -11,9 +11,9 @@ const appRoutes: Routes = [
     // ]},
     // { path: 'shopping-list', component: ShoppingListComponent},
     // { path: 'auth', component: AuthComponent }
-    {path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'},
-    {path: 'shopping-list', loadChildren: './shopping-list/shopping-list.module#ShoppingListModule'},
-    {path: 'auth', loadChildren: './auth/auth.module#AuthModule'}
+    {path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule)},
+    {path: 'shopping-list', loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule)},
+    {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)}
 ];
 
 @NgModule({
